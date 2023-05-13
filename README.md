54a186d34269fc648fa4f50d889bec4fd307adaf
CaoTianze <hnrcao@qq.com>
2023-05-13 15:18:53 +0800

    init

---
+++ README.md
@@ -0,0 +1,21 @@
+# pandasPGS: a Python package for easy retrieval of PGS Catalog data
+
+## Installation
+`pip install pandaspgs`
+## Example
+
+## Dependencies
+python: 3.10  
+pandas: 1.4.3  
+requests: 2.28.1  
+progressbar2: 4.0.0
+## Documentation
+See [pandasPGS Documentation](https://caotianze.github.io/pandaspgs/)
+## Licensing information
+### Source code
+MIT License
+### Data from NHGRI-EBI GWAS Catalog
+The NHGRI-EBI GWAS Catalog and all its contents are available under the general [Terms of Use for EMBL-EBI Services](https://www.ebi.ac.uk/about/terms-of-use). Summary statistics are made available under [CC0](https://creativecommons.org/publicdomain/zero/1.0/) unless [otherwise stated](https://www.ebi.ac.uk/gwas/docs/faq#faq-H7).
+## Development environment
+OS: Windows10 Professional  
+IDE: PyCharm 2022.1 (Community Edition)
---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pandaspgs",
+    version="0.9.0",
+    author="Cao Tianze",
+    author_email="hnrcao@qq.com",
+    description="A Python package for easy retrieval of PGS Catalog data",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/caotianze/pandaspgs",
+    project_urls={
+        "Bug Tracker": "https://github.com/caotianze/pandaspgs/issues",
+    },
+    classifiers=[
+        "Development Status :: 4 - Production/Stable",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=['pandaspgs'],
+    python_requires=">=3.10",
+    install_requires=['pandas>=1.4.3', 'requests>=2.28.1', 'progressbar2>=4.0.0'],
+    license="MIT",
+    keywords=['pgs', 'genomics', 'snp', 'bioinformatics','pandas'],
+    package_data={
+        "": ["*.csv","*.txt"]
+    }
+)
