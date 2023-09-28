abd8c351b4991e4af6645f9012e6de10ba9ace09
CaoTianze <hnrcao@qq.com>
2023-09-28 15:05:00 +0800

    TODO func get_traits

--- pandaspgs/get_trait.py
+++ pandaspgs/get_trait.py
@@ -1,6 +1,14 @@
 from typing import List, Dict
-from pandaspgs.client import get_trait_category
+from pandaspgs.client import get_trait_category, get_trait
 
 
-def get_trait_categories(cached=True) ->  List[Dict]:
-    return get_trait_category('https://www.pgscatalog.org/rest/trait_category/all',cached)
+def get_trait_categories(cached=True) -> List[Dict]:
+    return get_trait_category('https://www.pgscatalog.org/rest/trait_category/all', cached)
+
+
+def get_traits(trait_id: str = None, term: str = None, exact: bool = None, cached=True) -> List[Dict]:
+    pass
+
+
+def get_child_traits(trait_id: str = None, cached=True) -> List[Dict]:
+    pass
