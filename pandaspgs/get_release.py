071e20e50b83feb8b8d89067f73fc59ecbf7db63
CaoTianze <hnrcao@qq.com>
2023-11-10 20:34:34 +0800

    release url bugfix

--- pandaspgs/get_release.py
+++ pandaspgs/get_release.py
@@ -5,11 +5,11 @@ import re
 
 def get_releases(date: str = 'latest', cached=True) -> List[Dict]:
     if date == 'all':
-        return get_release('https://www.pgscatalog.org/rest/releases/all', cached=cached)
+        return get_release('https://www.pgscatalog.org/rest/release/all', cached=cached)
     if date == 'latest':
         return get_release('https://www.pgscatalog.org/rest/release/current', cached=cached)
     if date is None:
         raise Exception("Date can't be None.")
     if re.match('\\d{4}-\\d{2}-\\d{2}$', date) is None:
         raise Exception('The format of the date must be YYYY-MM-DD.')
-    return get_release('https://www.pgscatalog.org/rest/releases/rest/release/%s' % date, cached=cached)
+    return get_release('https://www.pgscatalog.org/rest/release/%s' % date, cached=cached)
--- tests/test_get_release.py
+++ tests/test_get_release.py
@@ -3,3 +3,5 @@ from pandaspgs.get_release import get_releases
 
 def test_get_releases():
     current = get_releases()
+    all_release = get_releases(date='all')
+    some_release = get_releases(date='2023-10-17')
