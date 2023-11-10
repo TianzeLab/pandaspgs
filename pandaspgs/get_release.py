94b6bba0d33380ae5f587bd4644817ca96c7b58a
CaoTianze <hnrcao@qq.com>
2023-11-10 20:21:22 +0800

    init test release

--- pandaspgs/get_release.py
+++ pandaspgs/get_release.py
@@ -7,7 +7,7 @@ def get_releases(date: str = 'latest', cached=True) -> List[Dict]:
     if date == 'all':
         return get_release('https://www.pgscatalog.org/rest/releases/all', cached=cached)
     if date == 'latest':
-        return get_release('https://www.pgscatalog.org/rest/releases/rest/release/current', cached=cached)
+        return get_release('https://www.pgscatalog.org/rest/release/current', cached=cached)
     if date is None:
         raise Exception("Date can't be None.")
     if re.match('\\d{4}-\\d{2}-\\d{2}$', date) is None:
---
+++ tests/test_get_release.py
@@ -0,0 +1,5 @@
+from pandaspgs.get_release import get_releases
+
+
+def test_get_releases():
+    current = get_releases()
