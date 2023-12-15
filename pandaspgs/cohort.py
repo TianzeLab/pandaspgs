74f2868cb91d1861c8ce4dace1d0efda165dd868
zzystc66 <145750716+zzystc66@users.noreply.github.com>
2023-12-16 01:28:27 +0800

    class C

--- pandaspgs/cohort.py
+++ pandaspgs/cohort.py
@@ -7,7 +7,7 @@ set_option('display.colheader_justify', 'center')
 set_option('display.precision', 3)
 
 
-class Publication:
+class Cohort:
     def __init__(self, data: list = [], mode: str = "Fat"):
         if data is None:
             data = []
@@ -19,34 +19,32 @@ class Publication:
             return
         if data is None or len(data) == 0:
             self.publications = DataFrame(
-                columns=['id', 'title', 'doi', 'PMID', 'journal', 'firstauthor', 'date_publication', 'date_release'
-                    , 'authors'])
+                columns=['name_short', 'name_full', 'name_others'])
             self.associated_pgs_ids = DataFrame(
-                columns=['publication_id', 'associated_pgs_id', 'stage'])
+                columns=['name_short', 'associated_pgs_id', 'stage'])
             return
-        datas = json_normalize(data=data, max_level=1).drop(columns=['id', 'title', 'doi', 'PMID', 'journal'
-            , 'firstauthor', 'date_publication', 'date_release', 'authors'])
+        datas = json_normalize(data=data, max_level=1).drop(columns=['name_short', 'name_full', 'name_others'])
         datas['associated_pgs_ids.development'] = datas['associated_pgs_ids.development'].map(lambda x: x == [])
         datas['associated_pgs_ids.evaluation'] = datas['associated_pgs_ids.evaluation'].map(lambda x: x == [])
-        self.publications = json_normalize(data=data, max_level=1).drop(
+        self.cohorts = json_normalize(data=data, max_level=1).drop(
             columns=['associated_pgs_ids.development', 'associated_pgs_ids.evaluation'])
         if not datas['associated_pgs_ids.development'].all() or not datas['associated_pgs_ids.evaluation'].all():
-            dva = json_normalize(data=data, record_path=['associated_pgs_ids', 'development'], meta=['id'])
-            eva = json_normalize(data=data, record_path=['associated_pgs_ids', 'evaluation'], meta=['id'])
+            dva = json_normalize(data=data, record_path=['associated_pgs_ids', 'development'], meta=['name_short'])
+            eva = json_normalize(data=data, record_path=['associated_pgs_ids', 'evaluation'], meta=['name_short'])
             dva['stage'] = 'development'
             eva['stage'] = 'evaluation'
             self.associated_pgs_ids = concat([dva, eva])
             self.associated_pgs_ids.columns = ['associated_pgs_id', 'name_short', 'stage']
         else:
             self.associated_pgs_ids = DataFrame(
-                columns=['associated_pgs_id', 'name_short', 'stage'])
+                 columns=['associated_pgs_id', 'name_short', 'stage'])
 
         return
 
     def __str__(self):
         if self.mode == 'Fat':
             return ("Cohort is running in fat mode. It has 2 DataFrames with hierarchical dependencies.\n"
-                    "publications: %d rows\n|\n -associated_pgs_ids: %d rows" % (len(self.publications),
+                    "publications: %d rows\n|\n -associated_pgs_ids: %d rows" % (len(self.cohorts),
                                                                                  len(self.associated_pgs_ids)))
         if self.mode == 'Thin':
             return ('Cohort is running in thin mode. It has 1 list that contains the raw data.\nraw_data: a list '
@@ -68,7 +66,7 @@ class Publication:
         raw_data = self.raw_data
         raw_data_dict = {}
         for j in raw_data:
-            raw_data_dict[j['id']] = j
+            raw_data_dict[j['name_short']] = j
         sub_set = []
         for i in arr:
             if isinstance(i, str):
@@ -77,11 +75,11 @@ class Publication:
                 sub_set.append(raw_data[i])
             else:
                 raise TypeError('Invalid item type: {}'.format(type(i)))
-        return Publication(sub_set, self.mode)
+        return Cohort(sub_set, self.mode)
 
     def __add__(self, other):
         if self.mode == other.mode:
-            return Publication(self.raw_data + other.raw_data, self.mode)
+            return self.raw_data + other.raw_data, self.mode
         else:
             raise Exception("Please input the same mode")
 
@@ -91,15 +89,15 @@ class Publication:
             self_dict = {}
             other_key_set = set()
             for i in self.raw_data:
-                self_key_set.add(i['id'])
-                self_dict[i['id']] = i
+                self_key_set.add(i['name_short'])
+                self_dict[i['name_short']] = i
             for j in other.raw_data:
-                other_key_set.add(j['id'])
+                other_key_set.add(j['name_short'])
             sub_key = self_key_set - other_key_set
             data = []
             for k in sub_key:
                 data.append(self_dict[k])
-            return Publication(data, self.mode)
+            return Cohort(data, self.mode)
         else:
             raise Exception("Please input the same mode")
 
@@ -109,15 +107,15 @@ class Publication:
             self_dict = {}
             other_key_set = set()
             for i in self.raw_data:
-                self_key_set.add(i['id'])
-                self_dict[i['id']] = i
+                self_key_set.add(i['name_short'])
+                self_dict[i['name_short']] = i
             for j in other.raw_data:
-                other_key_set.add(j['id'])
+                other_key_set.add(j['name_short'])
             sub_key = self_key_set & other_key_set
             data = []
             for k in sub_key:
                 data.append(self_dict[k])
-            return Publication(data, self.mode)
+            return Cohort(data, self.mode)
         else:
             raise Exception("Please input the same mode")
 
@@ -125,11 +123,11 @@ class Publication:
         if self.mode == other.mode:
             and_dict = {}
             for i in self.raw_data:
-                and_dict[i['id']] = i
+                and_dict[i['name_short']] = i
             for j in other.raw_data:
-                and_dict[j['id']] = j
+                and_dict[j['name_short']] = j
             data = list(and_dict.values())
-            return Publication(data, self.mode)
+            return Cohort(data, self.mode)
         else:
             raise Exception("Please input the same mode")
 
@@ -139,16 +137,16 @@ class Publication:
             and_dict = {}
             other_key_set = set()
             for i in self.raw_data:
-                self_key_set.add(i['id'])
-                and_dict[i['id']] = i
+                self_key_set.add(i['name_short'])
+                and_dict[i['name_short']] = i
             for j in other.raw_data:
-                other_key_set.add(j['id'])
-                and_dict[j['id']] = j
+                other_key_set.add(j['name_short'])
+                and_dict[j['name_short']] = j
             sub_key = self_key_set ^ other_key_set
             data = []
             for k in sub_key:
                 data.append(and_dict[k])
-            return Publication(data, self.mode)
+            return Cohort(data, self.mode)
         else:
             raise Exception("Please input the same mode")
 
--- pandaspgs/get_cohort.py
+++ pandaspgs/get_cohort.py
@@ -1,11 +1,15 @@
 from typing import List, Dict
 from pandaspgs.client import get_cohort
+from pandaspgs.cohort import Cohort
 
 
-def get_cohorts(cohort_symbol: str = None, cached=True) -> List[Dict]:
+def get_cohorts(cohort_symbol: str = None, cached=True, mode: str = 'Fat'):
     if cohort_symbol is None:
-        return get_cohort('https://www.pgscatalog.org/rest/cohort/all', cached=cached)
+        return Cohort(get_cohort('https://www.pgscatalog.org/rest/cohort/all', cached=cached),mode)
     by_id = None
     if cohort_symbol is not None:
         by_id = get_cohort('https://www.pgscatalog.org/rest/cohort/%s' % cohort_symbol, cached=cached)
-    return by_id
+    return Cohort(by_id, mode)
+
+
+print(get_cohorts())
