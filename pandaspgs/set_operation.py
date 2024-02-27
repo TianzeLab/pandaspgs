9c05860fa0602a951af8cba91c6a329eb8438964
zzystc66 <145750716+zzystc66@users.noreply.github.com>
2024-02-27 23:07:43 +0800

    2.27

--- pandaspgs/set_operation.py
+++ pandaspgs/set_operation.py
@@ -33,3 +33,133 @@ def bind(
 
     """
     return a + b
+
+
+def intersect(
+        a: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory,
+        b: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory) -> AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory:
+    """
+    Returns the data common to both A and B, with no repetitions
+
+    Args:
+        a: An object of the pandasPGS custom class.
+        b:An object of the same type as a.
+
+    Returns:
+        An object of the same type as a.
+    ```python
+    from pandaspgs.get_cohort import get_cohorts
+    from pandaspgs.set_operation import intersect
+
+
+    a = get_cohorts(cohort_symbol='100-plus')
+    b = get_cohorts(cohort_symbol='23andMe')
+    c = intersect(a,b)
+    ```
+
+    """
+    return a & b
+
+
+def set_diff(
+        a: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory,
+        b: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory) -> AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory:
+    """
+    returns the data in A that is not in B, with no repetitions
+
+    Args:
+        a: An object of the pandasPGS custom class.
+        b:An object of the same type as a.
+
+    Returns:
+        An object of the same type as a.
+    ```python
+    from pandaspgs.get_cohort import get_cohorts
+    from pandaspgs.set_operation import set_diff
+
+
+    a = get_cohorts(cohort_symbol='23andMe')
+    b = get_cohorts(cohort_symbol='23andMe')
+    c = set_diff(a,b)
+    ```
+
+    """
+    return a - b
+
+
+def set_xor(
+        a: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory,
+        b: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory) -> AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory:
+    """
+    returns the data of A and B that are not in their intersection (the symmetric difference), with no repetitions
+
+    Args:
+        a: An object of the pandasPGS custom class.
+        b:An object of the same type as a.
+
+    Returns:
+        An object of the same type as a.
+    ```python
+    from pandaspgs.get_cohort import get_cohorts
+    from pandaspgs.set_operation import set_xor
+
+
+    a = get_cohorts(cohort_symbol='100-plus')
+    b = get_cohorts(cohort_symbol='23andMe')
+    c = set_xor(a,b)
+    ```
+
+    """
+    return a ^ b
+
+
+def union(
+        a: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory,
+        b: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory) -> AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory:
+    """
+    returns the combined data from A and B with no repetitions
+
+    Args:
+        a: An object of the pandasPGS custom class.
+        b:An object of the same type as a.
+
+    Returns:
+        An object of the same type as a.
+    ```python
+    from pandaspgs.get_cohort import get_cohorts
+    from pandaspgs.set_operation import union
+
+
+    a = get_cohorts(cohort_symbol='100-plus')
+    b = get_cohorts(cohort_symbol='23andMe')
+    c = union(a,b)
+    ```
+
+    """
+    return a | b
+
+
+def set_equal(
+        a: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory,
+        b: AncestryCategory | Cohort | PerformanceMetric | Publication | Release | SampleSet | Score | Trait | TraitCategory) -> bool:
+    """
+    Check if the raw data of a and b are equal
+
+    Args:
+        a: An object of the pandasPGS custom class.
+        b:An object of the same type as a.
+
+    Returns:
+        An object of the same type as a.
+    ```python
+    from pandaspgs.get_cohort import get_cohorts
+    from pandaspgs.set_operation import set_equal
+
+
+    a = get_cohorts(cohort_symbol='100-plus')
+    b = get_cohorts(cohort_symbol='23andMe')
+    c = set_equal(a,b)
+    ```
+
+    """
+    return a == b
