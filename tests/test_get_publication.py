f9e804001e497a8899b42c49eaaf8240483245d2
CaoTianze <hnrcao@qq.com>
2025-01-08 10:48:42 +0800

    test reinit_cache

--- tests/test_get_publication.py
+++ tests/test_get_publication.py
@@ -1,5 +1,7 @@
 from pandaspgs.get_publication import get_publications
 from pandaspgs.client import clear_cache, reinit_cache
+from pandaspgs.get_trait import get_traits
+import pandaspgs.client as client
 
 def test_get_publications():
     filter_by_id = get_publications(pgp_id='PGP000001')
@@ -27,10 +29,17 @@ def test_get_publications():
     assert filter_by_id == filter_by_pgs_id
     assert len(filter_by_none[0:506] | filter_by_none[506]) == 507
 
+
 def test_pub_cache():
     pub = get_publications()
+    traits = get_traits()
+    reinit_cache(field='Publication', maxsize=10, ttl=10)
+    assert client.publication_cache.maxsize == 10
+    assert client.trait_cache.maxsize > 10
     pub = get_publications()
+    traits = get_traits()
     reinit_cache(field='All', maxsize=10, ttl=10)
     pub = get_publications()
-    reinit_cache(field='Publication', maxsize=10, ttl=10)
-    pub = get_publications()
+    traits = get_traits()
+    assert client.publication_cache.maxsize == 10
+    assert client.trait_cache.maxsize == 10
